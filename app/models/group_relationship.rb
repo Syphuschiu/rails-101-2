b050eff13ed89c8bbb2eb335398bedccc03ba813
Syphuschiu <antxhj@gmail.com>
2017-01-01 17:39:45 +0800

    implement group relation

--- app/models/group_relationship.rb
+++ app/models/group_relationship.rb
@@ -1,2 +1,4 @@
 class GroupRelationship < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_many :groups
   has_many :posts
   has_many :group_relationships
-  has_many :participated_group, :through => :group_relationships, :source => :group
+  has_many :participated_groups, :through => :group_relationships, :source => :group
 end
